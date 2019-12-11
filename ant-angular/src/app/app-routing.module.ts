2155380c37dc4357070d5e6495573e1b14323c0a
workingscorpion <workingscorpion@gmail.com>
2019-12-11 23:44:29 +0900

    feat : base of angular with ant-design

---
+++ ant-angular/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "/member" },
+  {
+    path: "member",
+    loadChildren: () =>
+      import("./pages/member/member.module").then(m => m.MemberModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ ant-angular/src/app/pages/member/member.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-member",
+  templateUrl: "./member.component.html",
+  styleUrls: ["./member.component.scss"]
+})
+export class MemberComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
